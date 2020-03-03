73fd721bb61779ebe9bbe95050c9183c31f29e14
danjo0418 <dj.degamo.p@gamil.com>
2020-03-03 17:59:28 +0800

    blog links

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,8 +38,8 @@
 			          	<a class="nav-link" href="{{ URL::to('our-agents') }}">Our Agents</a>
 			        </div>
 				</li>
-				<li class="nav-item {{ in_array(request()->segment(1), $news_url) ? 'active':''}}">
-			      	<a class="nav-link" href="{{ URL::to('news-and-updates') }}">News and Updates</a>
+				<li class="nav-item">
+			      	<a class="nav-link" href="https://blog.keylandrealty.com" target="_blank">News and Updates</a>
 				</li>
 				<li class="nav-item {{ in_array(request()->segment(1), $contact_url) ? 'active':''}}">
 			      	<a class="nav-link" href="{{ URL::to('contact-us') }}">Contact Us</a>
